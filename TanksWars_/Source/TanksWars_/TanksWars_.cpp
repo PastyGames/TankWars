fa3ca92a56c0369e2d4977745f7e7a17ba93ad81
DESKTOP-G8CMNJ3\dport <dportis68>
2018-03-01 16:57:11 -0600

    Setup Tank Files

---
+++ TanksWars_/Source/TanksWars_/TanksWars_.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TanksWars_.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TanksWars_, "TanksWars_" );
